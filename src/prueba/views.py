66fa9fde9184a0bd2f63c6158627d80e4f475711
Bryan Romero <bromerosite24@gmail.com>
2026-02-11 00:38:01 -0500

    arreglo

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    datos = {
-        "titulo": "Django",
-        "descripcin": "framework para crear aplicaciones web",
-    }
-    return render(request, "prueba/index.html", datos)
+    return render(request, "prueba/index.html", {"titulo": "python"})
