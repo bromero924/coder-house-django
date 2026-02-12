88c03576f04ca1eaa4263b0f21b0d74f14d32264
Bryan Romero <bromerosite24@gmail.com>
2026-02-12 00:36:19 -0500

    pruebas con plantilla

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "prueba/index.html", {"titulo": "python"})
+    datos = {
+        "titulo": "python",
+        "descripcion": "Frame work para crear aplicaciones web",
+    }
+    return render(request, "prueba/index.html", datos)
+
+
+def notas(request):
+    mis_notas = [10, 9, 8, 7, 6, 5]
+    return render(request, "prueba/notas.html", {"notas": mis_notas})
