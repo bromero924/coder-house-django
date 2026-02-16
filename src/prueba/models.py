349c816406fdd6a8961bc4ac6d09f6d0cc5ff7a3
Bryan Romero <bromerosite24@gmail.com>
2026-02-15 22:11:18 -0500

    registrar modelos en admin

--- src/prueba/models.py
+++ src/prueba/models.py
@@ -6,9 +6,15 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     pais_origen = models.ForeignKey(
         Pais, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.apellido.upper()},{self.nombre}"
